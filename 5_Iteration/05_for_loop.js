e1331af96fdafbc7a1d67965a6f363348a77699f
Abir-GBTech <asamanta@gbtechservice.com>
2024-11-05 18:58:44 +0530

    ..

--- 5_Iteration/05_for_loop.js
+++ 5_Iteration/05_for_loop.js
@@ -10,7 +10,7 @@ const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // const newNumbers = myNumbers.filter((num) => num > 4);
 // console.log(newNumbers);
 
-// Used Another Process with Return keywords.........
+// Used Another Process with Return keywords............
 
 const newNumbers = myNumbers.filter((num) => {
   return num > 4;
